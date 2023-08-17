d4e91476001f3bee2afde8f2a0d937d21c798f88
Amber Garcia <garamberr@gmail.com>
2023-08-17 09:18:31 -0400

    Add descriptive and regression table

--- EPI590R_FinalProj_AG.R
+++ EPI590R_FinalProj_AG.R
@@ -9,3 +9,20 @@ data(package = "medicaldata")
 #This data set contains data concerning testing for SARS-CoV2 via PCR as well as associated metadata.
 #The data has been anonymized, time-shifted, and permuted.
 covid_dataset <- medicaldata::covid_testing
+
+library(gtsummary)
+
+#Descriptive Table
+tbl_summary(
+	covid_dataset,
+	by = gender,
+	include = c(ct_result, result, payor_group, clinic_name)
+)
+
+#Regression Table
+tbl_uvregression(
+	covid_dataset,
+	y = ct_result,
+	include = c(gender, payor_group, clinic_name, result),
+	method = lm
+)
